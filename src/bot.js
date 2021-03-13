812cf2be9a4eed7b7d8081647556863090c71405
dazzlerkumar <dazzler.deepak98@gmail.com>
2021-03-13 13:15:58 +0530

    Update ver 1.0.0
    Removed Role feature and kept it to reply.

--- src/bot.js
+++ src/bot.js
@@ -1,21 +1,31 @@
 require("dotenv").config();
 
-const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('config.json','utf8'));
+
 const { Client } = require('discord.js');
 const client = new Client();
 
 client.login(process.env.SAGE_TOKEN);
-//client.on('ready', onReady );
-client.on('messageReactionAdd', addRole);
-client.on('messageReactionRemove', removeRole);
-
 
 client.on('message', message => {
-    // If the message is "ping"
-    if (message.content === 'ping') {
+    /*if (message.content === 'ping') {
       // Send "pong" to the same channel
       message.channel.send('pong');
+    }*/
+    let msg = message.content.toLowerCase();
+    switch(msg){
+      case 'ping': message.channel.send('pong');
+      break;
+      case 'chai': message.channel.send('Yeh Lo Chai!');
+      break;
+      case 'coffee': message.channel.send('Strong ya light ?');
+      break;
+      case 'biskut': message.channel.send('Yeh Lo Butterbite');
+      break;
+      case 'strong': message.channel.send('Yeh Lo Bru!!');
+      break;
+      case 'light': message.channel.send('Yeh Lo Nescafe!');
+      break;
+
     }
   });
 
@@ -29,84 +39,3 @@ client.on('guildMemberAdd', member => {
   });
 
 
-//_________________________________
-            //bpbuch's code
-//_________________________________
-/*
-  async function onReady() {
-    const channel = client.channels.find( ch => ch.name === config.channel);
-  
-    // channel will not contain messages after it is found
-    try {
-      await channel.messages.fetch();
-    } catch (err) {
-      console.error('Error fetching channel messages', err);
-      return;
-    }
-  
-    config.message_id = channel.messages.first().id;
-  
-    console.log(`Watching message '${config.message_id}' for reactions...`)
-  }
-  */
-// TO ADD ROLES
-async function addRole({message, _emoji}, user) {
-  if (user.bot || message.id !== config.message_id) {
-    return;
-  }
-  if (message.partial) {
-    try {
-      await message.fetch();
-    } catch (err) {
-      console.error('Error fetching message', err);
-      return;
-    }
-  }
-
-  const { guild } = message;
-
-  const member = guild.members.get(user.id);
-  const role = guild.roles.find((role) => role.name === config.roles[_emoji.name]);
-
-  if (!role) {
-    console.error(`Role not found for '${_emoji.name}'`);
-    return;
-  }
-  
-  try {
-    member.roles.add(role.id);
-  } catch (err) {
-    console.error('Error adding role', err);
-    return;
-  }
-}
-async function removeRole({message, _emoji}, user) {
-  if (user.bot || message.id !== config.message_id) {
-    return;
-  }
-  if (message.partial) {
-    try {
-      await message.fetch();
-    } catch (err) {
-      console.error('Error fetching message', err);
-      return;
-    }
-  }
-
-  const { guild } = message;
-
-  const member = guild.members.get(user.id);
-  const role = guild.roles.find((role) => role.name === config.roles[_emoji.name]);
-
-  if (!role) {
-    console.error(`Role not found for '${_emoji.name}'`);
-    return;
-  }
-
-  try{
-    member.roles.remove(role.id);
-  } catch (err) {
-    console.error('Error removing role', err);
-    return;
-  }
-}
